a41917cc1c952772f5fae8d11df008731d86486d
br0wnboi <akukreja987@gmail.com>
2023-07-03 07:02:57 +0530

    fix deauth

--- deauth-test/app.py
+++ deauth-test/app.py
@@ -6,10 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # Start packet capture on wlan0 interface
-    capture = pyshark.LiveCapture(interface='wlan0')
-
-    # Define a filter to capture only Wi-Fi management frames (subtype = 0x0C)
-    capture.set_display_filter('wlan.fc.type_subtype == 0x0C')
+    capture = pyshark.LiveCapture(interface='wlan0', bpf_filter='wlan.fc.type_subtype == 0x0C')
 
     # Initialize deauth attack counter
     deauth_count = 0
